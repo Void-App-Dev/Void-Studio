590e311991e3e9511874259eba3ecea02b4bbdda
Void Studio <yazn@voidstudio.ae>
2026-04-05 20:01:01 +0400

    Update and rename background_shader.js to backgroundShader.js

--- backgroundShader.js
+++ backgroundShader.js
@@ -372,7 +372,9 @@ function initGradflow(container) {
 
   requestAnimationFrame(frame);
 }
+  
 window.initGradflow = initGradflow;
+  
 // ---- WAIT FOR ELEMENT ----
 function wait(){
   const el = document.getElementById("gradflow");
